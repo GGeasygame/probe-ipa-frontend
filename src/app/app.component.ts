3b7007709864244a6b02d946422422966d53737f
Jonas Kaufmann <jonas.kaufmann@sunrise.ch>
2024-11-22 15:30:43 +0100

    removed useless comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'], // Fixed typo styleUrl -> styleUrls
+  styleUrls: ['./app.component.css'],
   standalone: true,
 })
 export class AppComponent implements OnInit {
-  name: string = ''; // To hold the user's input
-  response: string | undefined; // To display the response
+  name: string = '';
+  response: string | undefined;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    // Optional: Fetch the default response on initialization
     this.http.get('http://localhost:8080/hello', { responseType: 'text' })
       .subscribe({
         next: (response: string) => (this.response = response),
@@ -29,7 +27,6 @@ export class AppComponent implements OnInit {
   }
 
   sendName(): void {
-    // Make a request with the name provided by the user
     this.http.get(`http://localhost:8080/hello?myName=${encodeURIComponent(this.name)}`, { responseType: 'text' })
       .subscribe({
         next: (response: string) => (this.response = response),
